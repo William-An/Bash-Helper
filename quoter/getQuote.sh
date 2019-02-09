eb4578dcef6dcfa98cb2375304450ca1147f189e
William-An <China_Aisa@live.com>
2019-02-09 00:20:06 -0500

    Finish quote scrapyer

---
+++ quoter/getQuote.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Simple bash script that scrape the quote
+
+host=http://quotes.toscrape.com/random
+
+res=`curl $host | grep -e "<span"`
+res=`echo $res | cut -d ">" -f2 -f5 | cut -d "<" -f1 -f2 | sed "s/<\/span>/^/g"`
+
+quote=`cut -d "^" -f1 <<< $res`
+author=`cut -d "^" -f2 <<< $res`
+
+echo $quote
+echo $author
